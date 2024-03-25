448e42dd8f600ed7e1ee69d7b3edad0f95add572
Jackez01 <lucasgefreitas@gmail.com>
2024-03-25 12:26:49 -0300

    Corrigindo o Acúmulo de Timeout

---
+++ src/focusTimer/sounds.js
@@ -0,0 +1,7 @@
+export const buttonPressAudio = new Audio('./images/button-press.wav')
+
+export const kitchenTimer = new Audio('./images/kichen-timer.mp3')
+
+export const bgAudio = new Audio('./images/bg-audio.mp3')
+
+bgAudio.lopp = true
